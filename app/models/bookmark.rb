eb97ac52fe817f435579f5c7ae1d1821c8b904cf
Eduardo Barion <eduardobarion2004@hotmail.com>
2024-08-22 17:33:28 -0300

    Back-end feito

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,9 +2,8 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
 
+  validates :movie_id, uniqueness: { scope: :list_id }
+
   validates :comment, presence: true
   validates :comment, length: { minimum: 6 }
-
-  validates :movie, presence: true
-  validates :list, presence: true
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,16 +1,5 @@
 class Movie < ApplicationRecord
-  has_many :bookmarks, dependent: :restrict_with_error
-
-  before_destroy :check_for_bookmarks
+  has_many :bookmarks
 
   validates :title, :overview, presence: true
-
-  private
-
-  def check_for_bookmarks
-    if bookmarks.exists?
-      errors.add(:base, 'Cannot delete movie with associated bookmarks')
-      throw(:abort)
-    end
-  end
 end
